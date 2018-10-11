b56884eac7a4d0a4da7186cf3633d2bc1a924b95
Chloe Condon <misschloecondon@gmail.com>
2018-10-10 17:43:35 -0700

    backend clean-up

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -1,104 +1,104 @@
 package com.example.android.quizapp;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RadioGroup hamRadioGroup;
-    private RadioButton radioHamButton;
-    int quizScore = 0;
+    int correctAnswers = 0;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        submit = (Button) findViewById(R.id.submitButton);
+        submit.setOnClickListener(submitButtonOnClickListener);
     }
 
-    boolean answerEgot = false;
-    boolean answerRadio = false;
+    private String getQuestion1Input() {
+        EditText memoryAnswer = (EditText) findViewById(R.id.memory_answer);
+        String question1Answer = memoryAnswer.getText().toString();
+        return question1Answer;
+    }
 
+    private void checkQuestion1 () {
+        String question1Answer = getQuestion1Input();
+        if (question1Answer.equalsIgnoreCase("memory")) {
+            correctAnswers += 1;
+        }
 
-    public void submitQuiz(View view) {
-        EditText nameText = (EditText) findViewById(R.id.name_text);
-        String userName = nameText.getText().toString();
+    }
 
+    private void checkQuestion2 () {
+        RadioButton radioButtonMartha = (RadioButton) findViewById(R.id.radio_martha);
+        boolean isMarthaChecked = radioButtonMartha.isChecked();
+        if (isMarthaChecked) {
+            correctAnswers += 1;
+        }
 
-        hamRadioGroup = (RadioGroup) findViewById(R.id.hamilton_radio);
-        boolean answerRadio = getRadioAnswer(hamRadioGroup);
+    }
 
+    private String getQuestion3Input() {
         EditText lopezAnswer = (EditText) findViewById(R.id.lopez_answer);
-        String egotAnswer = lopezAnswer.getText().toString();
-        boolean answerEgot = getLopezAnswer(egotAnswer);
-
-        int finalScore = calculateScore(answerRadio, answerEgot);
-
-        String scoreMessage = createTotalMessage(finalScore, userName);
-        displayTotal(scoreMessage);
-
-
+        String fourLetterWord = lopezAnswer.getText().toString();
+        return fourLetterWord;
     }
 
-
-    private boolean getRadioAnswer(View view) {
-        int selectedId = hamRadioGroup.getCheckedRadioButtonId();
-        radioHamButton = (RadioButton) findViewById(selectedId);
-
-        if (radioHamButton == findViewById(R.id.radio_martha))    {
-            answerRadio = true;
+    private void checkQuestion3 () {
+        String fourLetterWord = getQuestion3Input();
+        if (fourLetterWord.equalsIgnoreCase("egot")) {
+            correctAnswers += 1;
         }
 
-        return  answerRadio;
     }
 
-    private boolean getLopezAnswer(String answer) {
-        if (answer == "EGOT"){
-            answerEgot = true;
+
+    private void checkQuestion4 (){
+        CheckBox Question3Simba = (CheckBox) findViewById(R.id.simba);
+        CheckBox Question3Ham = (CheckBox) findViewById(R.id.hamilton);
+        CheckBox Question3Usnavi = (CheckBox) findViewById(R.id.usnavi);
+        CheckBox Question3Sweeney = (CheckBox) findViewById(R.id.sweeney);
+
+        boolean isSimbaChecked = Question3Simba.isChecked();
+        boolean isHamiltonChecked = Question3Ham.isChecked();
+        boolean isUsnaviChecked = Question3Usnavi.isChecked();
+        boolean isSweeneyChecked = Question3Sweeney.isChecked();
+
+
+        if (isSimbaChecked && isSweeneyChecked && !isUsnaviChecked && !isHamiltonChecked) {
+            correctAnswers += 1;
         }
-        return  answerEgot;
-    }
-
-    private String createTotalMessage(int userTotal, String userName) {
-        String totalMessage = userName + "'s Total: " + userTotal;
-
-        return totalMessage;
     }
 
 
-    private int calculateScore(boolean answerRadio, boolean answerEgot ) {
+    private void checkQuestions() {
+        checkQuestion1();
+        checkQuestion2();
+        checkQuestion3();
+        checkQuestion4();
+    }
 
-        if (answerRadio) {
-            quizScore = quizScore + 1;
+    private void resetCounterCorrectAnswers(){
+        correctAnswers = 0;
+    }
+
+    final View.OnClickListener submitButtonOnClickListener = new View.OnClickListener() {
+        public void onClick(final View v){
+            checkQuestions();
+            Toast.makeText(MainActivity.this, "Correct Answers: " + correctAnswers + "/4",
+                    Toast.LENGTH_LONG).show();
+            resetCounterCorrectAnswers();
         }
-
-        if (answerEgot) {
-            quizScore = quizScore + 1;
-        }
-
-        return quizScore;
-
-    }
-
-    private void displayTotal (String message) {
-        TextView scoreMessage = (TextView) findViewById(R.id.total_score_text);
-        scoreMessage.setText(message);
-        scoreMessage.setVisibility(View.VISIBLE);
-    }
-
+    };
 }
-
-
-
-
-
-//
-//    private void displayTotal(String userTotal) {
-//        TextView userTotalDisplay = (TextView) findViewById(R.id.total_score_text);
-//        userTotalDisplay.setText("Score " + radioHamButton);
-//        userTotalDisplay.setVisibility(View.VISIBLE);
-//    }
